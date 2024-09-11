f8fc8f8e798190855a00d3cb6c235bff74761cdf
allwili <wilsonwu28@hotmail.com>
2024-09-11 16:09:27 -0300

    Cambion de puerto

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const configuration = {
     username: process.env.USERNAME ?? "Desconocido",
-    port: process.env.PORT ?? 3001,
+    port: process.env.PORT ?? 3002,
     apiKey: process.env.API_KEY ?? "Desconocida"
 }
